9552a46a7a775481309f73cd414de2369ac24964
marclar <mk@mk4p.com>
2014-01-21 16:21:48 -0500

    Updating main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ * main.js is responsible for reading the contents of the "models" directory and creating
+ *  models for sharing and syncing between the client and server
+ */
+function main(){}
+
+/**
+ * This method will search the given directory for models
+ * @param directory
+ */
+main.prototype.generate = function(directory){
+    //TODO
+};
+
+module.exports = new main();
